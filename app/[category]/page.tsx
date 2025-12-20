f4db24354e5621b5ec841aa3e4b0b1ab53f7292f
Abhishek Kumar <ahsekkumar@gmail.com>
2025-12-20 20:28:52 +0530

    api integration

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,53 +1,81 @@
+import Image from "next/image"
+
+
 type Article = {
   title: string
   description: string
   url: string
+  urlToImage?: string | null
+  source?: {
+    name?: string
+  }
 }
 
 async function getNews(category: string): Promise<Article[]> {
-  return [
-    {
-      title: `${category} News Headline 1`,
-      description: `This is a sample description for ${category} news.`,
-      url: "#",
-    },
-    {
-      title: `${category} News Headline 2`,
-      description: `Another important update from ${category}.`,
-      url: "#",
-    },
-  ]
+  const apiKey = process.env.NEWS_API_KEY
+
+  const res = await fetch(
+    `https://newsapi.org/v2/top-headlines?category=${category}&country=us&pageSize=12&apiKey=${apiKey}`,
+    { cache: 'no-store' }
+  )
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch news')
+  }
+
+  const data = await res.json()
+  return data.articles
 }
 
 
 type CategoryPageProps = {
-  params: {
+  params: Promise<{
     category: string
-  }
+  }>
 }
 
 export default async function CategoryPage({ params }: CategoryPageProps) {
-  const articles = await getNews(params.category)
+    const { category } = await params
+    const articles = await getNews(category)
+
 
   return (
     <main className="max-w-7xl mx-auto px-4">
       <h1 className="text-2xl font-bold mt-6 capitalize">
-        {params.category} News
+        {category} News
       </h1>
 
       <div className="grid gap-6 mt-6">
         {articles.map((article, index) => (
-          <div
-            key={index}
-            className="border rounded-lg p-4 hover:shadow"
-          >
-            <h2 className="text-lg font-semibold">
-              {article.title}
-            </h2>
-            <p className="text-gray-600 mt-2">
-              {article.description}
-            </p>
-          </div>
+            <div
+              key={index}
+              className="border rounded-lg overflow-hidden hover:shadow transition"
+            >
+              {article.urlToImage && (
+                <Image
+                  src={article.urlToImage}
+                  alt={article.title}
+                  width={400}
+                  height={250}
+                  className="w-full h-48 object-cover"
+                />
+              )}
+
+              <div className="p-4">
+                <h2 className="text-lg font-semibold">
+                  {article.title}
+                </h2>
+
+                <p className="text-gray-600 mt-2 text-sm">
+                  {article.description}
+                </p>
+
+                <p className="text-xs text-gray-400 mt-3">
+                  Source: {article.source.name}
+                </p>
+              </div>
+            </div>
+          
         ))}
       </div>
     </main>
